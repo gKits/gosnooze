640ac8693f825b9c7bf728ecb7b54029a6676d38
gkits <gkitsikoudis@gmx.de>
2025-07-30 08:34:44 +0200

    feat(runtime): add events for button holds

--- internal/runtime/default.go
+++ internal/runtime/default.go
@@ -31,7 +31,7 @@ func New(disp devices.Display, clock devices.Clock, buttons [3]devices.Button) *
 	}
 }
 
-func (run *Runtime) Tick() error {
+func (run *Runtime) Tick() (err error) {
 	e := run.getCurrentEvent()
 	switch e {
 	case EventNone:
@@ -40,7 +40,7 @@ func (run *Runtime) Tick() error {
 		} else {
 			run.disp.BacklightOn(false)
 		}
-	case EventAlarmFiring:
+	case EventAlarmFire:
 		run.mode = ModeShowTime
 		run.resetBuffer()
 		// TODO: Play alarm sound.
@@ -53,18 +53,24 @@ func (run *Runtime) Tick() error {
 
 	switch run.mode {
 	case ModeSetTime:
-		if err := run.setTime(e); err != nil {
-			run.mode = ModeShowTime
-			return err
-		}
-	case ModeSetAlarm:
+		err = run.setTime(e)
+	case ModeSetAlarm1:
+	case ModeSetAlarm2:
 	case ModeShowTime:
 		fallthrough
 	default:
-		if err := run.showTime(e); err != nil {
-			return err
-		}
+		err = run.showTime(e)
 	}
+
+	if errors.Is(err, modeFinished) {
+		run.mode = ModeShowTime
+		return nil
+	}
+
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -73,18 +79,24 @@ func (run *Runtime) showTime(e Event) error {
 	if err != nil {
 		return fmt.Errorf("failed to read time: %w", err)
 	}
-	run.disp.PrintTime(now)
+	run.disp.PrintTime(now, false)
 
 	switch e {
-	case EventButtonAPressed:
+	case EventButtonAPress:
 		if err := run.clock.EnableAlarm1(); err != nil {
 			return err
 		}
-	case EventButtonBPressed:
-	case EventButtonCPressed:
+	case EventButtonBPress:
+	case EventButtonCPress:
 		if err := run.clock.EnableAlarm2(); err != nil {
 			return err
 		}
+	case EventButtonAHold:
+		run.mode = ModeSetAlarm1
+	case EventButtonBHold:
+		run.mode = ModeSetTime
+	case EventButtonCHold:
+		run.mode = ModeSetAlarm2
 	}
 	return nil
 }
@@ -98,11 +110,11 @@ func (run *Runtime) setTime(e Event) (err error) {
 	}
 
 	switch e {
-	case EventButtonAPressed:
+	case EventButtonAPress:
 		run.timeBuf = modifyTimePosition(run.timeBuf, run.timeCur, -1)
-	case EventButtonCPressed:
+	case EventButtonCPress:
 		run.timeBuf = modifyTimePosition(run.timeBuf, run.timeCur, 1)
-	case EventButtonBPressed:
+	case EventButtonBPress:
 		run.timeCur++
 		if run.timeCur == TimeCursorOutOfBounds {
 			run.timeCur = 0
@@ -123,13 +135,13 @@ func (run *Runtime) setAlarm() error {
 func (run *Runtime) getCurrentEvent() Event {
 	switch {
 	case run.clock.IsAlarm1Fired(), run.clock.IsAlarm2Fired():
-		return EventAlarmFiring
+		return EventAlarmFire
 	case run.buttons[0].IsPressed():
-		return EventButtonAPressed
+		return EventButtonAPress
 	case run.buttons[1].IsPressed():
-		return EventButtonBPressed
+		return EventButtonBPress
 	case run.buttons[2].IsPressed():
-		return EventButtonCPressed
+		return EventButtonCPress
 	default:
 		return EventNone
 	}
