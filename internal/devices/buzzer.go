79bc60433aae39c0660331e4f1aba597a8bc0fe4
gkits <gkitsikoudis@gmx.de>
2025-08-06 23:39:13 +0200

    feat(devices): add Play method for buzzer

--- internal/devices/buzzer.go
+++ internal/devices/buzzer.go
@@ -2,6 +2,7 @@ package devices
 
 import (
 	"machine"
+	"time"
 
 	"tinygo.org/x/drivers/buzzer"
 )
@@ -10,6 +11,19 @@ type Buzzer struct {
 	buzzer.Device
 }
 
+type BuzzerNote struct {
+	Tone, Duration float64
+}
+
 func NewBuzzer(pin machine.Pin) Buzzer {
+	pin.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	pin.Set(true)
 	return Buzzer{buzzer.New(pin)}
+}
+
+func (buz *Buzzer) Play(song []BuzzerNote) {
+	for _, note := range song {
+		buz.Device.Tone(note.Tone, note.Duration)
+		time.Sleep(10 * time.Millisecond)
+	}
 }
